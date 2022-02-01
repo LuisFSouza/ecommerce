082ab2e2fa1a15273c16576fee8a012c6e40edc3
LuisFSouza <330luisfelipe@gmail.com>
2022-02-01 10:26:24 -0300

    Admin - Peridods - Busca e paginação

--- vendor/myclasses/php-classes/src/Model/Order.php
+++ vendor/myclasses/php-classes/src/Model/Order.php
@@ -103,5 +103,40 @@ class Order extends Model{
  {
      $_SESSION[Order::SUCCESS] = null;
  }
+
+ public static function getPage($page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page-1) * $itemsPerPage;
+
+        $sql = new Sql();
+        $results = $sql->select("select sql_calc_found_rows * from tb_orders a inner join tb_ordersstatus b using(idstatus) inner join tb_carts c using(idcart) inner join tb_users d on d.iduser = a.iduser inner join tb_addresses e using(idaddress) inner join tb_persons f on f.idperson = d.idperson order by a.dtregister desc limit $start, $itemsPerPage");
+
+        $resultTotal = $sql->select("select found_rows() as nrtotal");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultTotal[0]["nrtotal"],
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+        ];
+    }
+
+    public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page-1) * $itemsPerPage;
+
+        $sql = new Sql();
+        $results = $sql->select("select sql_calc_found_rows * from tb_orders a inner join tb_ordersstatus b using(idstatus) inner join tb_carts c using(idcart) inner join tb_users d on d.iduser = a.iduser inner join tb_addresses e using(idaddress) inner join tb_persons f on f.idperson = d.idperson where a.idorder = :id or f.desperson LIKE :searchlike order by a.dtregister desc limit $start, $itemsPerPage", [
+            ':searchlike'=>'%'.$search.'%',
+            ':id'=>$search
+        ]);
+
+        $resultTotal = $sql->select("select found_rows() as nrtotal");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultTotal[0]["nrtotal"],
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+        ];
+    }
 }
 
